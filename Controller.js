8db8ca77565a4d216f20c2e18c97b8e1e8a28a96
lihiitz <lihiitz@gmail.com>
2020-08-22 16:06:58 +0300

    no bugs, works great

--- Controller.js
+++ Controller.js
@@ -2,39 +2,27 @@ class Controller {
     constructor(elements, renderer) {
         this.elements = elements
         this.renderer = renderer
+        this.canClickButton = this.elements.length
     }
     run() {
 
         this.elements.forEach(e => this.renderer.renderElement(e, true))
-        $(`button`).on('click', function(){
-            elements.forEach(e => {
-                let id = setInterval(frame, e.speed)
-                function frame() {
-                    renderer.removeElement(e)
-                    let stop = e.move()
-                    renderer.renderElement(e)
-                    if (stop){
-                        clearInterval(id)
-                    }
-                }
-            })
-        
+        $(`button`).on('click', () => {
+            if (this.canClickButton === this.elements.length){
+                this.canClickButton = 0
+                this.elements.forEach(e => {
+                    let id = setInterval(() =>{
+                        this.renderer.removeElement(e)
+                        let stop = e.move()
+                        this.renderer.renderElement(e)
+                        if (stop){
+                            clearInterval(id)
+                            this.canClickButton++
+                        }
+                    }, e.speed)
+                })
+            }        
         })
-
-        // $(`button`).on('click', function(){
-        //     this.elements.forEach(e => {
-        //         let id = setInterval(frame, e.speed)
-        //         function frame() {
-        //             this.renderer.renderElement(e, false)
-        //             let stop = e.move(300)
-        //             this.renderer.renderElement(e, true)
-        //             if (stop){
-        //                 clearInterval(id)
-        //             }
-        //         }
-        //     })
-        
-        // })
     }
 
 }
--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
-
 const elements = [
-    new Circle(5, "red", 30, 30, 50, 0, 50),
+    new Circle(5, "green", 30, 30, 50, 0, 50),
     // new Circle({ speed: 5, color: "red", width: 30, height: 30, top: 50, left: 0, radius: 50 }),
 
-    new Square(10, "red", 50, 50, 100, 0),
-    new Triangle(8, "blue", 0, 0, 200, 0, 50, 50, 100)
+    new Square(10, "red", 50, 50, 50 + 50, 0),
+    new Triangle(8, "blue", 0, 0, 50 + 50 + 100, 0, 50, 50, 100)
 ]
 const renderer = new Render()
 const controller = new Controller(elements, renderer)
